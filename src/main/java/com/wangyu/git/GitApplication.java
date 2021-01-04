4917f63b277d412da6ad699f9dce95fcf21c2cc8
wangyu <wangyu9@bonc.com.cn>
2021-01-04 14:14:36 +0800

    提交代码

--- src/main/java/com/wangyu/git/GitApplication.java
+++ src/main/java/com/wangyu/git/GitApplication.java
@@ -8,8 +8,10 @@ public class GitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitApplication.class, args);
+		System.out.println("这是第一次改了哈哈哈1112");
 		System.out.println("这是第一次改了哈哈哈1");
-		System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hi");
 	}
 
 }
